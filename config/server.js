688f619ea8b8fa4657e5b151bcaec6a5792415ee
Smit <Smit@DESKTOP-UB9PN39>
2021-12-14 20:31:07 +0530

    first-commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'afd57bdc4f7afb8d48fa8b9c68dd436c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd1121411-5e94-4b0a-b911-7f28cb58ca16'
+};
